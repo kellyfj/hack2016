7884ec020838ffaef58389648a7d1a91888d26e1
Frank J Kelly <frank.kelly@here.com>
2016-06-06 11:10:06 -0400

    Update for watertown square

--- rest_search_for_stations.sh
+++ rest_search_for_stations.sh
@@ -1,5 +1,8 @@
 
-curl -v -G "https://cit.transit.api.here.com/search/by_geocoord.json?app_id=inhesa7azejETefrudAC&app_code=UP6A4YcFEAgshQMhc-sYsA&x&y=42.48&x=-71.19&radius=500&max=6"
+#Watertown Square Geo Coords
+lat=42.365813
+lon=-71.185237
+curl -v -G "https://cit.transit.api.here.com/search/by_geocoord.json?app_id=inhesa7azejETefrudAC&app_code=UP6A4YcFEAgshQMhc-sYsA&y=${lat}&x=${lon}&radius=500&max=20" | python -m json.tool 
 
-exit -1
+
 
