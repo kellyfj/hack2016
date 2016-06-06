94c498b79a80a4a32e8327c7f183a7fbfd75777c
Frank J Kelly <frank.kelly@here.com>
2016-06-06 11:04:48 -0400

    Update README and add Curl script to get Transit info

--- rest_search_for_stations.sh
+++ rest_search_for_stations.sh
@@ -1,13 +1,5 @@
 
-echo "Attempt #1"
-curl -v -G "https://transit.api.here.com/search/by_name?app_id=inhesa7azejETefrudAC&app_code=UP6A4YcFEAgshQMhc-sYsA&x=-73.99162351623576&y=40.751036894211815&max=5&name=New%20York"
+curl -v -G "https://cit.transit.api.here.com/search/by_geocoord.json?app_id=inhesa7azejETefrudAC&app_code=UP6A4YcFEAgshQMhc-sYsA&x&y=42.48&x=-71.19&radius=500&max=6"
 
-echo "Attempt #2"
-curl -v -G "https://cit.transit.api.here.com/search/by_name?app_id=inhesa7azejETefrudAC&app_code=UP6A4YcFEAgshQMhc-sYsA&x=-73.99162351623576&y=40.751036894211815&max=5&name=New%20York"
-
-echo "Attempt #3"
-curl -v -G --data-urlencode "app_id=inhesa7azejETefrudAC&app_code=UP6A4YcFEAgshQMhc-sYsA&x=-73.99162351623576&y=40.751036894211815&max=5&name=New%20York" "https://transit.api.here.com/search/by_name"
-
-echo "Attempt #4"
-curl -v -G --data-urlencode "app_id=inhesa7azejETefrudAC&app_code=UP6A4YcFEAgshQMhc-sYsA&x=-73.99162351623576&y=40.751036894211815&max=5&name=New%20York" "https://cit.transit.api.here.com/search/by_name"
+exit -1
 
